d243c70fe4597da149f13cd7756138a14562859e
masarom <romansanmaria@gmail.com>
2023-09-11 15:04:11 +0200

    fix button animation and start projects grid

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,44 @@
-import '../styles/layout/Projects.scss';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import ProjectsCard from './ProjectsCard';
+import "../styles/layout/Projects.scss";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ProjectsCard from "./ProjectsCard";
+import ImgInd1 from "../images/projects/rick-morty-react.png";
+import ImgInd2 from "../images/projects/friends-database-react.png";
+import ImgInd3 from "../images/projects/disney-database-javascript.png";
+import ImgInd4 from "../images/projects/dice-bet-javascript.png";
+import ImgInd5 from "../images/projects/layout-gulp.png";
 
 const Projects = () => {
   return (
-    <Container>
-      <Row>
-        <Col xs={12} md={6} lg={4}>
-          <ProjectsCard />
-        </Col>
-        <Col xs={12} md={6} lg={4}>
-          <ProjectsCard />
-        </Col>
-        <Col xs={12} md={6} lg={4}>
-          <ProjectsCard />
-        </Col>
-        <Col xs={12} md={6} lg={4}>
-          <ProjectsCard />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <h2>Adalab Projects</h2>
+        <h3>Individual Projects</h3>
+        <Row className="individual__projects">
+          <Col xs={12} md={6} lg={4}>
+            <ProjectsCard img={ImgInd1} title="Rick & Morty Database" footer="React"/>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <ProjectsCard img={ImgInd2} title="Friends Sentence Database" footer="React"/>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <ProjectsCard
+              img={ImgInd3}
+              title="Disney Characters Database" footer="JavaScript"
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <ProjectsCard img={ImgInd4} title="Dice and Bet Game" footer="JavaScript"/>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <ProjectsCard img={ImgInd5} title="Layout Project" footer="Gulp"/>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -1,18 +1,23 @@
-import '../styles/layout/Projects.scss';
+import "../styles/layout/Projects.scss";
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-const ProjectsCard = () => {
+const ProjectsCard = ({ img, title, footer }) => {
   return (
-    <Card style={{ width: '30rem' }}>
-      <Card.Img variant='top' src='holder.js/100px180' />
+    <Card style={{ width: "30rem" }}>
+      <Card.Img variant="top" src={img} className="card__img" />
       <Card.Body>
-        <Card.Title>Card Title 1</Card.Title>
+        <Card.Title>{title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
         </Card.Text>
-        <Button variant='primary'>Go somewhere</Button>
+        <Button variant="primary">Link</Button>
+        <Button variant="primary">Repo</Button>
+        <Card.Footer className="text-muted">
+          <Button>{footer}</Button>
+        </Card.Footer>
       </Card.Body>
     </Card>
   );
