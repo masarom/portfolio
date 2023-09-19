ae655508152bd8c8bb5a0c02d47b88c08fe5b6c1
masarom <romansanmaria@gmail.com>
2023-09-19 12:48:41 +0200

    add contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,18 @@
+import '../styles/layout/Contact.scss';
+
 const Contact = () => {
   return (
-<>
-Contact page</>
+<section className='contact'>
+  <h1 className='contact__title'>Send me a message</h1>
+  <form action="" className='contact__form'>
+      <label htmlFor="name" className='contact__form--label'>Name * </label><input type="text" className='contact__form--input' id='name' placeholder="Your name..." required/>
+      <label htmlFor="email" className='contact__form--label'> Email *</label><input type="email" className='contact__form--input' id="email" placeholder='your-email@mail.com'required/>
+      <label htmlFor="message" className='contact__form--label'>Your message:</label><textarea name="message" className='contact__form--textarea' id="message" cols="30" rows="10" placeholder='Your message here...'></textarea>
+  </form>
+  <div className="contact__other">
+    <p>Or get in touch with me through <a href="https://www.linkedin.com/in/maria-sanchez-roman/" className='contact__other--link' target='_blank' rel='noreferrer'>LinkedIn</a></p>
+  </div>
+</section>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import '../styles/layout/Main.scss';
 import MainHero from './MainHero';
 import Projects from './Projects';
