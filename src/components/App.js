956c1e7f26370712037a46fe61538bf54bbc1771
María Sánchez <msanchezroman95@gmail.com>
2023-09-20 13:28:26 +0200

    add notfound page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Main from './Main';
 import Contact from './Contact';
 import Footer from './Footer';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Main />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
         <Route path='/projects' element={<Main />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
       <Footer />
     </>
---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import '../styles/layout/NotFound.scss';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <section className='notfound'>
+      <h2 className='notfound__text'>Oops! This page doesn't exist. Try going</h2>
+      <Link to='/' className='notfound__link'>back home</Link>
+    </section>
+  );
+};
+
+export default NotFound;
